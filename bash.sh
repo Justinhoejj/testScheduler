14a3a60fa9d2b36bca037ba52d62053cfe7e0866
justinhoejj <hoejj05@gmail.com>
2021-12-24 21:50:54 +0800

    Updated on Fri Dec 24 13:50:54 UTC 2021

--- bash.sh
+++ bash.sh
@@ -1,12 +1,9 @@
 #! /bin/bash
 
-#echo $(
-# /usr/local/bin/python3 /Users/justinhoe/Desktop/Projects/scheduledTask/update.py
-#)
-
 echo $(
+/usr/local/bin/python3 /Users/justinhoe/Desktop/Projects/scheduledTask/update.py
 cd /Users/justinhoe/Desktop/Projects/scheduledTask
 git add --all
-git commit -m "$(date)"
+git commit -m "Updated on $(date -u)"
 git push
 )
